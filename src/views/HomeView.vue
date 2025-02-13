079c72da8f5ad29a84f877f0ad2c7787b5b91f54
Meylis <meylisg98@gmail.com>
2025-02-13 21:18:29 +0500

    banner done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,8 +9,40 @@
   <main>
     <Hero />
 
+    <section class="banner mt-60 ">
+      <div class="auto_container">
+        <div class="wrapper">
+          <div class="block bg-[url('@/assets/images/banner.png')] py-16 rounded-[20px]">
+            <h4 class="text-3xl font-extrabold text-white text-center max-w-[950px] mx-auto mb-5">
+              Flights, Hotels, and Exclusive Deals – Just a click Away!
+              Contact us 24/7 for B2B Offers.
+            </h4>
+            <div class="flex items-center justify-center gap-[10px]">
+              <div class="flex items-center">
+                <span class="block mr-2">
+                  <img src="@/assets/images/whatsapp.png" alt="media-icon">
+                </span>
+                <p class="text-xl font-extrabold text-white bg-[#43C653] py-4 px-5 rounded-lg">
+                  +993 65 123456
+                </p>
+              </div>
+
+              <div class="flex items-center">
+                <span class="block mr-2">
+                  <img src="@/assets/images/telegram.png" alt="media-icon">
+                </span>
+                <p class="text-xl font-extrabold text-white bg-[#27A7E7] py-4 px-5 rounded-lg">
+                  +993 65 123456
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <!-- <Tour /> -->
-    <section class="tours mt-60">
+    <section class="tours mt-[60px]">
       <div class="auto_container">
         <div class="wrapper relative">
           <h2 class=" text-[32px] text-prime-color font-semibold mb-4">
