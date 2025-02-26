ed0e320563e526a028796d675e525224cea41166
Amanmyrat <tekemuradov@gmail.com>
2025-02-26 17:14:23 +0500

    header locale - new components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,32 +4,10 @@ import { RouterLink, useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/userStore';
 import logo from "@/assets/images/logo.png";
 import apiService from '@/services/apiService';
+import Locale from '@/components/Locale.vue'
 
-import { useI18n } from "vue-i18n";
-const { locale } = useI18n();
 const router = useRouter();
 const userStore = useUserStore();
-
-// Locale selection
-const selectedLocale = ref(localStorage.getItem('locale') || 'en');
-const dropdownVisible = ref(false);
-
-const localeLabels = {
-    en: "English",
-    ru: "Russian",
-    tk: "Turkmen",
-};
-
-const toggleDropdown = () => {
-    dropdownVisible.value = !dropdownVisible.value;
-};
-
-const changeLocale = (newLocale) => {
-    selectedLocale.value = newLocale;
-    localStorage.setItem('locale', newLocale);
-    locale.value = newLocale; // Update Vue I18n
-    dropdownVisible.value = false; // Close dropdown
-};
 
 const user = ref('');
 
@@ -61,7 +39,6 @@ onMounted(async () => {
 
 
     userStore.loadUser();
-    locale.value = selectedLocale.value;
 
     if (userStore.user) {
         try {
@@ -122,6 +99,9 @@ onBeforeUnmount(() => {
                                         $t("header.links.profile") }}
                                 </RouterLink>
                             </li>
+                            <li class="flex justify-center lg:hidden relative min-w-52 rounded-lg shadow-md">
+                                <Locale class="w-full text-center"/>
+                            </li>
                         </ul>
                     </nav>
 
@@ -137,19 +117,7 @@ onBeforeUnmount(() => {
                             {{ $t("header.signin") }}
                         </RouterLink>
 
-                        <div class="block">
-                            <button @click="toggleDropdown"
-                                class="py-2 px-4 border border-[#223A60] text-sm sm:text-base text-center rounded-lg bg-white text-[#223A60] cursor-pointer">
-                                {{ localeLabels[selectedLocale] }}
-                            </button>
-                            <ul v-if="dropdownVisible"
-                                class="absolute left-0 mt-2 w-full bg-white border border-[#223A60] rounded-lg shadow-md">
-                                <li v-for="(label, locale) in localeLabels" :key="locale" @click="changeLocale(locale)"
-                                    class="p-2 text-center cursor-pointer hover:bg-gray-200">
-                                    {{ label }}
-                                </li>
-                            </ul>
-                        </div>
+                        <Locale class="hidden lg:block" />
 
                         <button @click="toggleMenu" class="burger block lg:hidden xsm:ml-4">
                             <svg class="block object-contain" width="24" height="18" viewBox="0 0 24 18" fill="none"
---
+++ src/components/Locale.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import { useUserStore } from '@/stores/userStore';
+
+// import Locale from 'v-calendar/dist/types/src/utils/locale';
+const { locale } = useI18n();
+const userStore = useUserStore();
+
+// Locale selection
+const selectedLocale = ref(localStorage.getItem('locale') || 'en');
+const dropdownVisible = ref(false);
+
+const localeLabels = {
+    en: "English",
+    ru: "Russian",
+    tk: "Turkmen",
+};
+
+const toggleDropdown = () => {
+    dropdownVisible.value = !dropdownVisible.value;
+};
+
+const changeLocale = (newLocale) => {
+    selectedLocale.value = newLocale;
+    localStorage.setItem('locale', newLocale);
+    locale.value = newLocale; // Update Vue I18n
+    dropdownVisible.value = false; // Close dropdown
+};
+
+onMounted(async () => {
+    userStore.loadUser();
+    locale.value = selectedLocale.value;
+});
+</script>
+
+<template>
+
+    <div class="block">
+        <button @click="toggleDropdown"
+            class="py-2 px-4 w-full border border-[#223A60] text-sm sm:text-base text-center rounded-lg bg-white text-[#223A60] cursor-pointer">
+            {{ localeLabels[selectedLocale] }}
+        </button>
+        <ul v-if="dropdownVisible"
+            class="absolute left-0 mt-2 w-full bg-white border border-[#223A60] rounded-lg shadow-md">
+            <li v-for="(label, locale) in localeLabels" :key="locale" @click="changeLocale(locale)"
+                class="p-2 text-center cursor-pointer hover:bg-gray-200">
+                {{ label }}
+            </li>
+        </ul>
+    </div>
+
+</template>
